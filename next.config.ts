cdff9a677f8319ef1c7f108a2b8787b47575d07f
mtomlet <kramtelmot@gmail.com>
2025-08-10 22:41:04 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
