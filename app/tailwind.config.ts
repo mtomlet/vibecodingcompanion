0350da2583560f87f71d4e6bdf38f82899300fb9
mtomlet <kramtelmot@gmail.com>
2025-08-10 22:54:49 -0400

    Create tailwind.config.ts

---
+++ app/tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
